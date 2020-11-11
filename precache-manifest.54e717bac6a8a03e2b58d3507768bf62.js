22a9dccbe03cb74811b2109c903c3807263465f3
Denis Chumakov <chumakov@azoft.com>
2020-11-11 13:42:20 +0700

    'deploy'

---
+++ precache-manifest.54e717bac6a8a03e2b58d3507768bf62.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a2c292042e693b3adf188b9eff22f49",
+    "url": "/events/open-3/images/rpa.png"
+  },
+  {
+    "revision": "78f96ad16309533476a59a9e6f9f212a",
+    "url": "/events/open-3/Правки по тексту ПКЛ_ребрендинг (new).pptx"
+  },
+  {
+    "revision": "0ff1bc4d14e5c9abcacba7c600d97814",
+    "url": "/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "4035eb944f80025d6e05",
+    "url": "/js/app.b30473e9.js.map"
+  },
+  {
+    "revision": "f22d501a35a87d9f21701cb031f6ea17",
+    "url": "/img/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "cb4ef0fc6ccab281269a",
+    "url": "/js/chunk-vendors.1727c87f.js"
+  },
+  {
+    "revision": "f130a0b70e386170cf6f011c0ca8c4f4",
+    "url": "/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "cb4ef0fc6ccab281269a",
+    "url": "/js/chunk-vendors.1727c87f.js.map"
+  },
+  {
+    "revision": "8c6de712ea9aac1bca65569b2e91bda8",
+    "url": "/img/blue-yellow.8c6de712.png"
+  },
+  {
+    "revision": "cde26b25491bb984e26c76ac7362c9d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "8f3f064e889c1da3d2f097b5ea711e8e",
+    "url": "/img/PlusGreen.svg"
+  },
+  {
+    "revision": "8e4a3852e1d87264efe3ef5dcbcbe19a",
+    "url": "/img/YouTube.svg"
+  },
+  {
+    "revision": "058a3335d15a3eb84e7ae3707ba09620",
+    "url": "/img/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "b89032a4a5a1879f30ba05a13947f26f",
+    "url": "/img/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "98b614336d9a12cb3f7bedb001da6fca",
+    "url": "/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "66830ea6be8e7e94fb55df9f7b778f2e",
+    "url": "/img/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "4bb1a55479d61843b89a2fdafa7849b3",
+    "url": "/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "af28d69d59284dd202aa55e57227b11b",
+    "url": "/img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "c43cd371a49ee4ca17ab3a60e72bdd51",
+    "url": "/img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "9a2b5c0f19de617685b7b5b42464e7db",
+    "url": "/img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "1a034e64d80905128113e5272a5ab95e",
+    "url": "/img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "936d6e411cabd71f0e627011c3f18fe2",
+    "url": "/img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "1ba2ae710d927f13d483fd5d1e548c9b",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "46bc69048b59bb79de5e78c84cdf683a",
+    "url": "/events/pairs-1/images/YouTube.svg"
+  },
+  {
+    "revision": "ca80c3e2d1dc062f5a24b367fbfe9dcb",
+    "url": "/events/pairs-1/images/azoft.svg"
+  },
+  {
+    "revision": "4035eb944f80025d6e05",
+    "url": "/js/app.b30473e9.js"
+  },
+  {
+    "revision": "21f4752eb889d7f7ec3bab0f7359f3fa",
+    "url": "/events/open-1/images/one.inc.png"
+  },
+  {
+    "revision": "46bc69048b59bb79de5e78c84cdf683a",
+    "url": "/events/open-3/images/YouTube.svg"
+  },
+  {
+    "revision": "0eafd988d1247988f964191fd7f1e21b",
+    "url": "/events/open-3/images/itk.png"
+  },
+  {
+    "revision": "5c0df5e96307287fbc103a3acc41a6f5",
+    "url": "/events/open-3/images/ds1.png"
+  },
+  {
+    "revision": "ca80c3e2d1dc062f5a24b367fbfe9dcb",
+    "url": "/events/open-3/images/azoft.svg"
+  },
+  {
+    "revision": "21f4752eb889d7f7ec3bab0f7359f3fa",
+    "url": "/events/open-3/images/one.inc.png"
+  },
+  {
+    "revision": "ca80c3e2d1dc062f5a24b367fbfe9dcb",
+    "url": "/events/open-1/images/azoft.svg"
+  },
+  {
+    "revision": "4a2c292042e693b3adf188b9eff22f49",
+    "url": "/events/open-2/images/rpa.png"
+  },
+  {
+    "revision": "60e5f4d08bdb501a7ec6ed7ce343f6e7",
+    "url": "/events/open-3/Vue-Socket.io-3.0.10.zip"
+  },
+  {
+    "revision": "267ed8c0b512ae222284227604e58e3c",
+    "url": "/events/open-3/CopperGoldSilverGradients-20110505.svg"
+  },
+  {
+    "revision": "5c0df5e96307287fbc103a3acc41a6f5",
+    "url": "/events/open-2/images/ds1.png"
+  },
+  {
+    "revision": "ca80c3e2d1dc062f5a24b367fbfe9dcb",
+    "url": "/events/open-2/images/azoft.svg"
+  },
+  {
+    "revision": "0eafd988d1247988f964191fd7f1e21b",
+    "url": "/events/open-2/images/itk.png"
+  },
+  {
+    "revision": "21f4752eb889d7f7ec3bab0f7359f3fa",
+    "url": "/events/open-2/images/one.inc.png"
+  },
+  {
+    "revision": "cb4ef0fc6ccab281269a",
+    "url": "/css/chunk-vendors.6e31e61f.css"
+  },
+  {
+    "revision": "5c0df5e96307287fbc103a3acc41a6f5",
+    "url": "/events/open-1/images/ds1.png"
+  },
+  {
+    "revision": "4c4f2a16c766f32cf8b31bec089bbcde",
+    "url": "/events/open-1/photos/1st.png"
+  },
+  {
+    "revision": "da6b0dbfcf899ef78b4d3d2e1e6ab4d7",
+    "url": "/events/open-1/photos/3rd.png"
+  },
+  {
+    "revision": "46bc69048b59bb79de5e78c84cdf683a",
+    "url": "/events/open-2/images/YouTube.svg"
+  },
+  {
+    "revision": "0eafd988d1247988f964191fd7f1e21b",
+    "url": "/events/open-1/images/itk.png"
+  },
+  {
+    "revision": "46bc69048b59bb79de5e78c84cdf683a",
+    "url": "/events/open-1/images/YouTube.svg"
+  },
+  {
+    "revision": "cb4ef0fc6ccab281269a",
+    "url": "/css/chunk-vendors.6e31e61f.css.map"
+  },
+  {
+    "revision": "4035eb944f80025d6e05",
+    "url": "/css/app.6b4bd993.css.map"
+  },
+  {
+    "revision": "4035eb944f80025d6e05",
+    "url": "/css/app.6b4bd993.css"
+  },
+  {
+    "revision": "72733e8e9bf02594d2a8880344aef243",
+    "url": "/CNAME"
+  }
+];
